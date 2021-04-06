61a8b86363a21838f7fabd56e33b49a0873ae520
lucaasraamirez <80787727+lucaasraamirez@users.noreply.github.com>
2021-04-06 01:01:49 -0300

    Add files via upload

---
+++ Ej1-Promedio.c
@@ -0,0 +1,23 @@
+//promedio
+#include <stdio.h>
+
+int main() {
+	int e=0,suma=0, valor;
+	float promedio;
+	
+	printf("ingrese los valores que quiere promediar. Para termnar ingresar 9999\n");
+	
+	while (1) 
+	{
+		scanf("%d",&valor);
+		if(valor==9999)
+		{
+			promedio = suma / e;
+			printf("El promedio es:%.2f",promedio);
+			break;
+		}
+		suma=suma+valor;
+		e++;
+	}
+	return 0;
+}
---
+++ Ej2-InterésCompuesto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double monto;
+	double principal=1000.0;
+	double tasa;
+	int anio;
+	
+	for ( tasa=.05;tasa<0.11;tasa+=0.01) 
+	{
+		printf("%4s %21s %20s\n","Anio","Monto del deposito","Tasa");
+		for (anio=1;anio<=10;anio++) 
+		{
+			monto=principal*pow(1.0+tasa,anio);
+			printf("%4d %21.2f %20.2f\n",anio,monto,tasa);
+		}	
+	}
+	return 0;
+}
